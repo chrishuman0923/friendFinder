06b3ea7c88c402b1955df380017876add52a8adb
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-11-12 20:54:05 -0500

    Adds functionality to construct object from POST data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,29 @@
 //define constants of the application
 const express = require('express'),
     app = module.exports = express(),
-    friends = require('../data/friends');
+    friends = require('../data/friends'),
+    friendData = friends;
 
 //enable express middleware    
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes for application data
-app.get('/api/friends', (req, res) => res.json(friends));
+app.get('/api/friends', (req, res) => res.json(friendData));
 
-app.post('/api/friends', (req, res) => console.log(req.body.nameInput, req.body.question1));
+app.post('/api/friends', function (req, res) {
+    //Get submitted data and create a new object
+    var postData = Object.values(req.body),
+        input = {
+            name: postData[0].trim(),
+            photo: postData[1].trim(),
+            scores: []
+        };
+    
+    //Add scores to new object
+    for (var i = 2; i < postData.length; i ++) {
+        input.scores.push(parseInt(postData[i]));
+    }
+
+    friendData.push(input);
+});
